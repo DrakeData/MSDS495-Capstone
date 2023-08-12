26aad2a31c988a6737f45586bd230087bd8c80c7
drakedata <nick@drakedata.io>
2023-08-12 08:03:28 -0500

    Updated images

--- app/pages/2_User_Data.py
+++ app/pages/2_User_Data.py
@@ -369,7 +369,7 @@ with st.container():
         #     st.warning("Make sure you set the correct CLIENT_ID and CLIENT_SECRET.")
 
 # Ad
-st.image("https://github.com/DrakeData/MSDS498-Capstone/blob/main/app/listr_premium_ad2.png")
+st.image("listr_premium_ad2.png")
 
 left_col, center_col, right_col = st.columns([2, 1, 2])
 with left_col:
